13002fd39fbe886809a5bc24942070e7de5e5144
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-17 18:38:57 +0100

    Commit initial

---
+++ src/OtroWhile.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class OtroWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        JOptionPane.showInputDialog("Ingrese el primer numero");
+
+
+
+    }
+}
