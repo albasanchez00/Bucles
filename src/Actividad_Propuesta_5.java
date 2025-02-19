107ac95a901999316436458da92dbda952cc5cfc
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-19 20:26:21 +0100

    Actividad Propuesta 5 y Bucles Dependientes

---
+++ src/Actividad_Propuesta_5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/* Actividad Propuesta 5.
+*
+*
+*
+*/
+
+public class Actividad_Propuesta_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
