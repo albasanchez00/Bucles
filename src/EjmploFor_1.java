63dbea723ad97e1fd57d7c148d58d07fc825494d
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-02-19 18:39:03 +0100

    Ejemplo 1 Bucle For

--- src/EjmploFor_1.java
+++ src/EjmploFor_1.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-/* Ejemplo 1. Vamos a solicitar al usuario un número, u mostrara todos los números del 1 hasta
-   el número ingresado
-*/
-
 public class EjmploFor_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        /* Ejemplo 1. Vamos a solicitar al usuario un número, u mostrara todos los números del 1 hasta
+           el número ingresado.*/
         int num;
         System.out.print("Indique un número → ");
         num = sc.nextInt();
@@ -16,13 +15,13 @@ public class EjmploFor_1 {
 
 
         /* Ejemplo 2. Escribe un programa que muestre todos los multiplos de 7, menores que 100 */
-        int maximo=100;
-        System.out.println("\n");
-        for (int a=0;a<=maximo;a+=7) {
+        int multiplo;
+        System.out.println("\nIngrese un multiplo →");
+        multiplo = sc.nextInt();
+        System.out.printf("Multiplos de %d hasta 100 →\n",multiplo);
+        for (int a=0;a<=100;a+=multiplo) {
             System.out.printf("%d-",a);
         }
-
-
-
+        sc.close();
     }
 }
